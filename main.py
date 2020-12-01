9cff1114bb79b3b68c70eeeb2e7493f551a02ef8
Marija Erceg <marija.erceg1@gmail.com>
2020-12-01 14:28:48 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/about")
+def about_me():
+    return render_template("about_me.html")
+
+@app.route("/portfolio")
+def portfolio():
+    return render_template("portfolio.html")
+
+@app.route("/portfolio/fakebook")
+def fakebook():
+    return render_template("fakebook.html")
+
+@app.route("/portfolio/boogle")
+def boogle():
+    return render_template("boogle.html")
+
+@app.route("/portfolio/hairsalon")
+def hairsalon():
+    return render_template("hairsalon.html")
+
+
+if __name__ == '__main__':
+    app.run()
