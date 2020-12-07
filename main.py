829770b480eb3288384bbdc0e9d959b62f7c9aed
Marija Erceg <marija.erceg1@gmail.com>
2020-12-07 11:02:25 +0100

    pokušaj Jinja - ne radi base.css

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def index():
-    return render_template("index.html")
+def base():
+    return render_template("base.html")
 
 @app.route("/about")
 def about_me():
