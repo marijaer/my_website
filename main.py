fb31043035dd3e78894d1dc1d3a92d129ab9e70b
Marija Erceg <marija.erceg1@gmail.com>
2020-12-08 10:20:15 +0100

    dodavanje index.html

--- main.py
+++ main.py
@@ -2,10 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route("/")
-def base():
-    return render_template("base.html")
+def index():
+    return render_template("index.html")
 
 @app.route("/about")
 def about_me():
